6bfa8f4dff1f2d9fbb0474239781a5ca83b569ae
Your Name <you@example.com>
2025-10-24 08:30:06 +0700

    dashboard

--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  BookOpen,
+  ClipboardCheck,
+  RotateCcw,
+  SquareArrowOutUpLeft,
+} from "lucide-react";
 
-function nav() {
+export default function Nav() {
+  const [active, setActive] = useState("manager");
+  const navigate = useNavigate();
+
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const handleClick = () => {
+    console.log(active);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
+
   return (
-    <div>
+    <nav className="w-[15vw] min-w-[230px] h-screen bg-gradient-to-b from-[#41826e] to-[#2e5f50] shadow-lg flex flex-col">
+      <div className="w-full h-24 border-b border-white/30 flex flex-col justify-center items-center">
+        <h1 className="text-white font-bold text-3xl tracking-wide uppercase">
+          SU Library
+        </h1>
+        <p className="text-white/70 text-sm mt-1">Admin Panel</p>
+      </div>
 
-    </div>
-  )
+      <div className="flex flex-col gap-3 p-4 mt-4">
+        <button
+          onClick={() => setActive("manager")}
+          className="flex items-center gap-3 px-4 py-2 border border-white/30 rounded-lg text-white hover:bg-white/20 hover:border-white/60 transition-all duration-200"
+        >
+          <BookOpen className="w-5 h-5" />
+          <span>จัดการหนังสือ</span>
+        </button>
+
+        <button
+          onClick={() => setActive("Approval")}
+          className="flex items-center gap-3 px-4 py-2 border border-white/30 rounded-lg text-white hover:bg-white/20 hover:border-white/60 transition-all duration-200"
+        >
+          <ClipboardCheck className="w-5 h-5" />
+          <span>อนุมัติการยืม</span>
+        </button>
+
+        <button
+          onClick={() => setActive("return")}
+          className="flex items-center gap-3 px-4 py-2 border border-white/30 rounded-lg text-white hover:bg-white/20 hover:border-white/60 transition-all duration-200"
+        >
+          <RotateCcw className="w-5 h-5" />
+          <span>อนุมัติการคืน</span>
+        </button>
+      </div>
+
+      <div className="flex flex-col gap-3 p-4 mt-4 absolute bottom-10">
+        <button onClick={handleLogout} className="flex items-center gap-3 px-4 py-2 border border-white/30 rounded-lg text-white hover:bg-white/20 hover:border-white/60 transition-all duration-200">
+          <SquareArrowOutUpLeft className="w-5 h-5" />
+          <span>ออกจากระบบ</span>
+        </button>
+      </div>
+
+      <div className="mt-auto mb-6 absolute bottom-5 left-12   text-center text-white/50 text-xs">
+        © 2025 SU Library
+      </div>
+    </nav>
+  );
 }
-
-export default nav
--- frontend/src/page/admin/dashbord.jsx
+++ frontend/src/page/admin/dashbord.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Nav from "../../components/nav";
 
 function Dashboard() {
 
   const navigate = useNavigate();
 
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem("user"));
 
-  const handleLogout = () => {
-
-    localStorage.removeItem('user');
-    navigate('/login');
-  }
-
+  useEffect(() => {
+    if(!user) {
+      navigate("/login");
+    }
+  });
+  
   return (
-    <div>
-      <h1>Welcome to Dashboard</h1>
-      <button onClick={handleLogout} className='border p-5 bg-red-600 text-white rounded-lg'>
-        Logout
-      </button>
+    <div className="w-screen h-screen bg-amber-200 flex justify-center items-center">
+      <Nav />
+      <div className="w-[85vw] h-screen bg-red-300 px-8 py-10">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum ipsa
+        iure iste! Saepe a laboriosam voluptate nam excepturi quas consequuntur
+        voluptas? Dicta aperiam optio velit totam quasi, laboriosam natus
+        asperiores placeat doloremque, ex cupiditate laborum provident a,
+        perspiciatis quos ullam ratione. Corrupti dolores optio, hic a magni
+        accusamus totam ut dolor accusantium reprehenderit molestias enim
+        laborum eius quidem repellendus porro praesentium recusandae. Non
+        commodi magnam sit doloribus, suscipit, dolorum repudiandae nobis illum
+        quaerat pariatur quod voluptate, nemo dolores quos amet quasi blanditiis
+
+        quod.
+      </div>
     </div>
-  )
+  );
 }
 
 export default Dashboard;
