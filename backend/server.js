1d79ca1e08ef1c89fcf57958790c45084b724788
Chanon Hongthong <hongthong_c@su.ac.th>
2025-11-12 01:25:51 +0700

    add some code to server test

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req , res) => {
-    res.json({message: 'welcome to server'})
+    res.json({message: 'welcome to server!'})
 });
 
 app.post('/login' , (req ,res) => {
